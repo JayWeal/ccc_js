33998aebdaae26756e8bd13324ea357a2c9e6a5f
Bülent <buelent_@hotmail.com>
2024-04-17 10:15:20 +0300

    foods added

---
+++ s03/data/constants.js
@@ -0,0 +1,62 @@
+export const foods = [
+    {
+        "name": "Avocado",
+        "type": "Fruit",
+        "healthindicator": "A"
+    },
+    {
+        "name": "Curcumber",
+        "type": "Veggie",
+        "healthindicator": "A"
+    },
+    {
+        "name": "Bluebarries",
+        "type": "Fruit",
+        "healthindicator": "A"
+    },
+    {
+        "name": "French Fries",
+        "type": "Veggie",
+        "healthindicator": "D"
+    },
+    {
+        "name": "Schnitzel",
+        "type": "Meat",
+        "healthindicator": "E"
+    },
+    {
+        "name": "Past",
+        "type": "Veggie",
+        "healthindicator": "C"
+    },
+    {
+        "name": "Burger",
+        "type": "Meat",
+        "healthindicator": "D"
+    },
+    {
+        "name": "Joghurt",
+        "type": "Dairy",
+        "healthindicator": "B"
+    },
+    {
+        "name": "Pizza",
+        "type": "Veggie",
+        "healthindicator": "C"
+    },
+    {
+        "name": "Spring Rolls",
+        "type": "Veggie",
+        "healthindicator": "A"
+    },
+    {
+        "name": "Steak",
+        "type": "Meat",
+        "healthindicator": "D"
+    },
+    {
+        "name": "Tomato Soupe",
+        "type": "Veggie",
+        "healthindicator": "B"
+    }
+]
